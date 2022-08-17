140a974da4f02e49d7effb6cd6fb83c037a4191f
Brenden Vogt <bvogtdev@gmail.com>
2022-08-16 23:57:01 -0700

    Added a hello world brenden example

--- ggcli/commands/hello.py
+++ ggcli/commands/hello.py
@@ -9,6 +9,10 @@ def helloworld_main(args):
     print(f"hello world: {args}")
 
 
+def hello_brenden(args):
+    print(f"Hello Brenden its nice to see you again")
+
+
 command = Command(
     name="hello",
     index=hello_main,
@@ -17,7 +21,13 @@ command = Command(
             name="world",
             func=helloworld_main,
             help="Ah the wonderful 'hello world' function"
-        )
+        ),
+        Subcommand(
+            name="brenden",
+            func=hello_brenden,
+            help="a hello just for Brenden"
+        ),
+
     ],
     help="This is the good old 'hello' function"
 )
